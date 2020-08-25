f8bd6da61bbd3232922ca4820cb52f7b1de0b48b
Sergey-Sukmanyuk <ssukmanyuk@gmail.com>
2020-08-25 21:51:25 +0300

    Irish Store on Magento 2

---
+++ src/dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,15 @@
+
+'use strict';
+
+module.exports = {
+    customtheme: {
+        area: 'frontend',
+        name: 'Mage/customtheme',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    }
+};
